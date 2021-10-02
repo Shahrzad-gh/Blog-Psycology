b1e62d4f86ba19aa274df5f281d3fde4e1b0cf6c
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-02 17:52:04 +0330

    implement router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import "./App.css";
 //import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import { ConfigProvider } from "antd";
+import Article from "./components/Home/Article/Article";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <ConfigProvider direction="rtl">
       <div className="App">
         {/* <Navbar /> */}
-        <Home />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/article" component={Article} />
+          </Switch>
+        </Router>
       </div>
     </ConfigProvider>
   );
