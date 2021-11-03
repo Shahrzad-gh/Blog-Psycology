81a331df06f9b66ce338044cb49ca612a46d05a2
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-03 19:46:49 +0330

    add sign in - out links to navbar

---
+++ src/components/Navbar/SignInLinks.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SignInLinks() {
+  return (
+    <ul>
+      <li>
+        <img
+          className="avatar"
+          src="https://image.freepik.com/free-photo/modern-woman-taking-selfie_23-2147893976.jpg"
+          alt="avatar"
+          title="name family"
+        />
+      </li>
+      <li className="btn">خروج</li>
+    </ul>
+  );
+}
+
+export default SignInLinks;
---
+++ src/components/Navbar/SignOutLinks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignOutLinks() {
+  return (
+    <ul>
+      <li className="btn">ورود</li>
+    </ul>
+  );
+}
+
+export default SignOutLinks;
