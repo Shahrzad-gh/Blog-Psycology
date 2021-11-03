6fc7b1e6052ecc2e44eb4ff82f1c406700897d0c
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-03 19:46:15 +0330

    delete antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import "./App.css";
-//import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { ConfigProvider } from "antd";
-import Article from "./components/Home/Article/Article";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <ConfigProvider direction="rtl">
-      <div className="App">
-        {/* <Navbar /> */}
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/article" component={Article} />
-          </Switch>
-        </Router>
-      </div>
-    </ConfigProvider>
+    <div className="App">
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
