e2bb396dd89808333b8b635dc7a1b336074ab5f8
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-04 22:01:46 +0330

    edit navabr position

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Article from "./components/Article/Article";
+import AddPost from "./components/Article/AddPost";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/article" component={Article} />
+          <Route path="/add" component={AddPost} />
         </Switch>
       </Router>
     </div>
