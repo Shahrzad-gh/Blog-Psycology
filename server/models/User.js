d55cf3bfef4c05d7fe217987244f83a0d753bba7
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-07 21:18:04 +0330

    add user- model

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    descripton: {
+      type: String,
+      maxlength: 160,
+    },
+  },
+  { timestamps: true }
+);
+
+// static method to login user
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
