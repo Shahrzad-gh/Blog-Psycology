a89abbcac1ea7d40922dc661e4c28366680daaf7
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-08 21:07:41 +0330

    update category

---
+++ server/Controllers/Categories.js
@@ -0,0 +1,11 @@
+const Category = require("../models/Category");
+
+module.exports.addCat_post = (req, res) => {};
+
+module.exports.removeCat_delete = (req, res) => {};
+
+module.exports.editCat_put = (req, res) => {};
+
+module.exports.getAllCats_get = (req, res) => {};
+
+module.exports.getCatById_get = () => {};
--- server/Routes/Categories.js
+++ server/Routes/Categories.js
@@ -1,1 +1,11 @@
 const { Router } = require("express");
+const categoryController = require("../Controllers/Categories");
+const router = Router();
+
+router.post("/add", categoryController.addCat_post);
+router.delete("/remove", categoryController.removeCat_delete);
+router.put("/edit", categoryController.editCat_put);
+router.get("/getall", categoryController.getAllCats_get);
+router.get("/get/:id", categoryController.getCatById_get);
+
+module.exports = router;
