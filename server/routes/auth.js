844d14214e9c29830b3ac3021b68f80b201279c7
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-08 21:07:58 +0330

    update auth route - register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,6 @@ const authController = require("../Controllers/Auth");
 const router = Router();
 
 router.post("/signin", authController.login_post);
+router.post("/register", authController.register_post);
 
 module.exports = router;
