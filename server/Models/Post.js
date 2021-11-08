e0c9369ecfffdd5304160b3ae6d543f371d77044
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-08 21:07:10 +0330

    update model

--- server/Models/Post.js
+++ server/Models/Post.js
@@ -1,3 +1,31 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({});
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("post", PostSchema);
+module.exports = Post;
