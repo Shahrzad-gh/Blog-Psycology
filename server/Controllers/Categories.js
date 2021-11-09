aeeb9bf646b88ec294ec777141c10302c24e526f
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-09 13:24:34 +0330

    add - new category - get all

--- server/Controllers/Categories.js
+++ server/Controllers/Categories.js
@@ -1,11 +1,26 @@
-const Category = require("../models/Category");
+const Category = require("../Models/Category");
 
-module.exports.addCat_post = (req, res) => {};
+module.exports.addCat_post = async (req, res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).json(savedCat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-module.exports.removeCat_delete = (req, res) => {};
+// module.exports.removeCat_delete = (req, res) => {};
 
-module.exports.editCat_put = (req, res) => {};
+// module.exports.editCat_put = (req, res) => {};
 
-module.exports.getAllCats_get = (req, res) => {};
+module.exports.getAllCats_get = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 module.exports.getCatById_get = () => {};
