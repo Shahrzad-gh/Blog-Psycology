d3a88eacffae0f294e9b6354fe7f5077815c0a83
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-09 13:24:17 +0330

    edite category route

--- server/Routes/Categories.js
+++ server/Routes/Categories.js
@@ -3,8 +3,8 @@ const categoryController = require("../Controllers/Categories");
 const router = Router();
 
 router.post("/add", categoryController.addCat_post);
-router.delete("/remove", categoryController.removeCat_delete);
-router.put("/edit", categoryController.editCat_put);
+// router.delete("/remove", categoryController.removeCat_delete);
+// router.put("/edit", categoryController.editCat_put);
 router.get("/getall", categoryController.getAllCats_get);
 router.get("/get/:id", categoryController.getCatById_get);
 
