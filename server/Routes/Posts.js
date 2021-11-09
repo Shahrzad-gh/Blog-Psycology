6b9917d28196d8e625b6a73b04398d52d8ac42b4
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-09 13:21:51 +0330

    edit Post routes

--- server/Routes/Posts.js
+++ server/Routes/Posts.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/add", postController.addPost_post);
 router.delete("/remove/:id", checkUser, postController.removePost_delete);
 router.put("/edit/:id", checkUser, postController.editPost_put);
-router.get("/getalls", postController.getAllPosts_get);
-router.get("/getbyid", postController.getPostById_get);
+router.get("/getall", postController.getAllPosts_get);
+router.get("/get/:id", postController.getPostById_get);
 
 module.exports = router;
