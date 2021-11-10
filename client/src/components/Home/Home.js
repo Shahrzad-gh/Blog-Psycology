41a8f7ce23db296992b3b4d04eae2a60eebe094c
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-10 19:51:02 +0330

    update

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,25 +1,16 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 import Header from "../Header/Header";
 import Posts from "../Posts/Posts";
 import Sidebar from "../Sidebar/Sidebar";
-import { getPost } from "../../redux/postSlice";
 import "./Home.css";
 
 function Home() {
-  const post = useSelector((state) => state.post.data);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPost());
-  }, []);
-
-  console.log(post);
   return (
     <>
       <Header />
+
       <div className="home">
-        <Posts data={post} />
+        <Posts />
         <Sidebar />
       </div>
     </>
