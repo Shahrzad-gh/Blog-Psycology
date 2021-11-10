215eb392af53be1f77fd240e1051be2f6195af1c
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-10 19:51:37 +0330

    get data by redux toolkit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Post from "./Post";
+import { useSelector } from "react-redux";
+import { useGetAllPostsQuery } from "../../redux/postsApi";
 
 function Posts() {
-  // console.log(props);
+  const {
+    // items: posts,
+    status,
+  } = useSelector((state) => state.posts);
+
+  const {
+    data,
+    // error, isLoading
+  } = useGetAllPostsQuery();
+
   return (
     <div className="posts">
-      {/* {post.map((p) => (
-        <Post data={p} />
-      ))} */}
+      {status === "success" ? (
+        <>{data && data?.map((post) => <Post key={post._id} post={post} />)}</>
+      ) : status === "pending" ? (
+        <p>Loading...</p>
+      ) : (
+        <p>Unexpected error occured...</p>
+      )}
     </div>
   );
 }
