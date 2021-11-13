757a24b95fca0df4c84a001c9da29b164c0940ed
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-13 16:09:49 +0330

    test add search

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,15 +2,20 @@ import React from "react";
 import Header from "../Header/Header";
 import Posts from "../Posts/Posts";
 import Sidebar from "../Sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
+
 import "./Home.css";
 
 function Home() {
+  const { search } = useLocation();
+  console.log(search);
+  // const { cat } = location ? location.state : null;
   return (
     <>
       <Header />
 
       <div className="home">
-        <Posts />
+        <Posts search={search} />
         <Sidebar />
       </div>
     </>
