0ca97807dd361ebd500922d0a6ddc4bf4e760005
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-14 19:22:51 +0330

    add edit route

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Article from "./components/Article/Article";
 import AddPost from "./components/Article/AddPost";
 import SignIn from "./components/signIn/SignIn";
 import Settings from "./pages/Settings";
-
+import Edit from "./components/Posts/Edit";
 function App() {
   const user = true;
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/post/:postId" component={Article} />
           <Route path="/create">{user ? <AddPost /> : <SignIn />}</Route>
+          <Route path="/edit">{user ? <Edit /> : <SignIn />}</Route>
           <Route path="/signin" component={SignIn} />
           <Route path="/settings">{user ? <Settings /> : <SignIn />}</Route>
         </Switch>
