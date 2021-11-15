8c919f050123351deb36aae57c26c54d5378b69d
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-15 20:00:52 +0330

    test add auth redux toolkit

---
+++ client/src/redux/authSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { register, login, logout } from "./auth";
+//import cookie from "react-cookie";
+import { useCookies } from "cookie-parser";
+
+const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+const user = cookies.get("token");
+
+const initialState = user
+  ? { isLoggedIn: true, user }
+  : { isLoggedIn: false, user: null };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: {
+    [register.fulfilled]: (state, action) => {
+      state.isLoggedIn = false;
+    },
+    [register.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload.user;
+    },
+    [login.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+    [logout.fulfilled]: (state, action) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+  },
+});
+
+const { reducer } = authSlice;
+export default reducer;
