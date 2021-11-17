ed579c0b72bd608b94a790f8259d1f0ee07898c5
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-17 16:24:09 +0330

    test - update login with redux toolkit

--- client/src/components/signIn/SignIn.js
+++ client/src/components/signIn/SignIn.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, userSelector } from "../../redux/authSlice";
 import "./SignIn.css";
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { isFetching, isSuccess, isError, errorMessage } =
+    useSelector(userSelector);
+
+  const handleOnChange = (e) => {
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+  console.log(loginInfo);
+
+  const handleSignin = (e) => {
+    console.log(loginInfo);
+    dispatch(loginUser(loginInfo));
+  };
+
   return (
     <div className="signin">
-      <form className="signinForm">
+      <form className="signinForm" onSubmit={handleSignin}>
         <h1>ورود</h1>
-        <input type="text" id="username" placeholder="نام کاربری" />
-        <input type="password" id="password" placeholder="رمز ورود" />
-        <button className="signinButton">ورود</button>
+        <input
+          type="email"
+          id="email"
+          placeholder="ایمیل"
+          name="email"
+          onChange={handleOnChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="رمز ورود"
+          name="password"
+          onChange={handleOnChange}
+        />
+        <button type="submit" className="signinButton">
+          ورود
+        </button>
       </form>
     </div>
   );
