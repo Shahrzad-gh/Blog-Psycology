60d3ec84f41c069bc51fd71b46f63ec5c9346420
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-18 18:45:35 +0330

    add token to cookies

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,30 @@ import AddPost from "./components/Article/AddPost";
 import SignIn from "./components/signIn/SignIn";
 import Settings from "./pages/Settings";
 import Edit from "./components/Posts/Edit";
+import axios from "axios";
+import { AuthContextProvider } from "./context/authContext";
+
+//to save token in cookies
+axios.defaults.withCredentials = true;
 
 function App() {
   const user = false;
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/post/:postId" component={Article} />
-          <Route path="/create">{user ? <AddPost /> : <SignIn />}</Route>
-          <Route path="/edit">{user ? <Edit /> : <SignIn />}</Route>
-          <Route path="/signin" component={SignIn} />
-          <Route path="/settings">{user ? <Settings /> : <SignIn />}</Route>
-        </Switch>
-      </Router>
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/post/:postId" component={Article} />
+            <Route path="/create">{user ? <AddPost /> : <SignIn />}</Route>
+            <Route path="/edit">{user ? <Edit /> : <SignIn />}</Route>
+            <Route path="/signin" component={SignIn} />
+            <Route path="/settings">{user ? <Settings /> : <SignIn />}</Route>
+          </Switch>
+        </Router>
+      </div>
+    </AuthContextProvider>
   );
 }
 
