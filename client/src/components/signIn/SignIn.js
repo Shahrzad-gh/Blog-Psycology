f99baabb4df626fc823ed2a25edc45d58c89f397
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-18 18:59:09 +0330

    test

--- client/src/components/signIn/SignIn.js
+++ client/src/components/signIn/SignIn.js
@@ -33,32 +33,32 @@ function SignIn() {
 
   return (
     <div className="signin">
-      {isSuccess ? (
+      {/* {isSuccess ? (
         <Redirect to="/" />
-      ) : (
-        <form className="signinForm" onSubmit={handleSignin}>
-          <h1>ورود</h1>
-          <input
-            type="email"
-            id="email"
-            placeholder="ایمیل"
-            name="email"
-            onChange={handleOnChange}
-          />
-          <input
-            type="password"
-            id="password"
-            placeholder="رمز ورود"
-            name="password"
-            onChange={handleOnChange}
-          />
-          <button type="submit" className="signinButton">
-            ورود
-          </button>
-        </form>
-      )}
+      ) : ( */}
+      <form className="signinForm" onSubmit={handleSignin}>
+        <h1>ورود</h1>
+        <input
+          type="email"
+          id="email"
+          placeholder="ایمیل"
+          name="email"
+          onChange={handleOnChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="رمز ورود"
+          name="password"
+          onChange={handleOnChange}
+        />
+        <button type="submit" className="signinButton">
+          ورود
+        </button>
+      </form>
+      {/* )}
       {isError ? <p>{errorMessage}</p> : null}
-      {isFetching ? <p>Loading</p> : null}
+      {isFetching ? <p>Loading</p> : null} */}
     </div>
   );
 }
