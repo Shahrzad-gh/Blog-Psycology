fb715370fdd78c4e29d1980282d05f530bb2b1ac
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-19 16:19:36 +0330

    update url

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -2,20 +2,23 @@
 import React, { useEffect, useState, createContext } from "react";
 import axios from "axios";
 
-const AuthContext = createContext();
+const AuthContext = createContext("");
 
 function AuthContextProvider(props) {
   const [isLoggedIn, setisLoggedIn] = useState(false);
   const [user, setUser] = useState("user");
 
+  const URL = "http://localhost:8080/api/auth";
   async function getLoggedIn() {
     try {
-      const loggedInRes = await axios.get("/loggedIn");
+      const loggedInRes = await axios.get(`${URL}/loggedIn`);
+      console.log(loggedInRes);
       setisLoggedIn(loggedInRes.data);
 
-      const userRes = await axios.get("/getuser");
-      setUser(userRes.data);
-      console.log(isLoggedIn);
+      // const userRes = await axios.get(`${URL}/getuser`);
+      // setUser(userRes.data);
+      // console.log("isLoggedIn", isLoggedIn);
+      // console.log("user", user);
     } catch (err) {
       console.log(err);
     }
@@ -23,7 +26,7 @@ function AuthContextProvider(props) {
 
   useEffect(() => {
     getLoggedIn();
-  }, []);
+  });
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, getLoggedIn, user }}>
