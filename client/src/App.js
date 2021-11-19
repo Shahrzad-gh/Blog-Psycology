006c57f86edec2536da80c389364513119027b1c
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-19 16:20:54 +0330

    add authcontext values

--- client/src/App.js
+++ client/src/App.js
@@ -9,24 +9,31 @@ import Settings from "./pages/Settings";
 import Edit from "./components/Posts/Edit";
 import axios from "axios";
 import { AuthContextProvider } from "./context/authContext";
+import authContext from "./context/authContext";
+import { useContext } from "react";
 
 //to save token in cookies
 axios.defaults.withCredentials = true;
 
 function App() {
-  const user = false;
+  const { isLoggedIn, user } = useContext(authContext);
+
   return (
-    <AuthContextProvider>
+    <AuthContextProvider value={isLoggedIn}>
       <div className="App">
         <Router>
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/post/:postId" component={Article} />
-            <Route path="/create">{user ? <AddPost /> : <SignIn />}</Route>
-            <Route path="/edit">{user ? <Edit /> : <SignIn />}</Route>
+            <Route path="/create">
+              {isLoggedIn ? <AddPost /> : <SignIn />}
+            </Route>
+            <Route path="/edit">{isLoggedIn ? <Edit /> : <SignIn />}</Route>
             <Route path="/signin" component={SignIn} />
-            <Route path="/settings">{user ? <Settings /> : <SignIn />}</Route>
+            <Route path="/settings">
+              {isLoggedIn ? <Settings /> : <SignIn />}
+            </Route>
           </Switch>
         </Router>
       </div>
