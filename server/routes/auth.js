44daef16180d2eeb9057146cfd8ece29453b926d
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-20 14:47:17 +0330

    add logout route - server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,7 @@ const router = Router();
 router.post("/signin", authController.login_post);
 router.post("/register", authController.register_post);
 router.get("/loggedIn", authController.loggedIn_get);
+router.get("/signout", authController.logout_get);
 router.get("/getuser", authController.getUser);
 
 module.exports = router;
