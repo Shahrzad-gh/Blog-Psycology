fce0c49e66610bc8968a385db8aac26ec8a70a13
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-21 20:00:47 +0330

    add and edit post with photo

--- server/Controllers/Posts.js
+++ server/Controllers/Posts.js
@@ -1,21 +1,39 @@
 const Post = require("../Models/Post");
 const cloudinary = require("../utils/cloudinary");
+const handleError = (error) => {
+  let err = { message: "" };
+
+  if (error.code === 400) {
+    err.message = "File size too large";
+  }
+};
 
 module.exports.addPost_post = async (req, res) => {
-  // let photo = { img: "", id: "" };
-  // cloudinary.config({
-  //   cloud_name: process.env.CLOUDINARY_CLOUDNAME,
-  //   api_key: process.env.CLOUDINARY_APIKEY,
-  //   api_secret: process.env.CLOUDINARY_APISECRET,
-  // });
-
-  //const newPost = new Post(req.body);
-
+  let photo = { img: "", id: "" };
+  const { title, author, desc, categories } = req.body;
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_APISECRET,
+  });
   try {
-    // const result = await cloudinary.uploader.upload(req.file.path);
-    // photo.img = result.secure_url;
-    // photo.id = result.public_id;
-    const newPost = await Post.create(req.body);
+    if (req.file !== undefined) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      photo.img = result.secure_url;
+      photo.id = result.public_id;
+    } else {
+      photo = {
+        img: "https://res.cloudinary.com/dw8wf8gps/image/upload/v1637507302/default-text-effect_67638-192_sgvqyk.jpg",
+        id: "",
+      };
+    }
+    const newPost = await Post.create({
+      title,
+      author,
+      desc,
+      categories,
+      photo,
+    });
     res.status(200).json(newPost);
   } catch (err) {
     res.status(500).json(err);
@@ -43,25 +61,42 @@ module.exports.removePost_delete = async (req, res) => {
 };
 module.exports.editPost_put = async (req, res) => {
   const postId = req.params.id;
+  let photo = { img: "", id: "" };
+  const { title, author, desc, categories } = req.body;
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_APISECRET,
+  });
   try {
     const post = await Post.findById(postId);
+
     if (post.author.toLowerCase() === res.locals.user.username.toLowerCase()) {
       try {
+        if (req.file !== undefined) {
+          const result = await cloudinary.uploader.upload(req.file.path);
+          photo.img = result.secure_url;
+          photo.id = result.public_id;
+        } else {
+          photo = { img: post.photo.img, id: post.photo.id };
+        }
         const newPost = await Post.findByIdAndUpdate(
           postId,
           {
-            $set: req.body,
+            $set: { title, author, desc, categories, photo },
           },
           { new: true }
         );
         res.status(201).json(newPost);
       } catch (error) {
-        res.status(500).json(error);
+        const err = handleError(error);
+        res.status(500).json(err);
       }
     } else {
       res.status(401).json("you should update your posts");
     }
   } catch (error) {
+    console.log("2", error);
     res.status(500).json(error);
   }
 };
