3d68830f12a0177cd67361a2023d4c9ad9024c4e
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-22 19:07:59 +0330

    update photo - user name - to navbar

--- client/src/components/Navbar/SignInLinks.js
+++ client/src/components/Navbar/SignInLinks.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/authSlice";
-function SignInLinks() {
+function SignInLinks({ photo, username }) {
   const dispatch = useDispatch();
   const handleSignout = (e) => {
     e.preventDefault();
@@ -11,12 +11,23 @@ function SignInLinks() {
   return (
     <ul>
       <li>
-        <img
-          className="avatar"
-          src="https://image.freepik.com/free-photo/modern-woman-taking-selfie_23-2147893976.jpg"
-          alt="avatar"
-          title="name family"
-        />
+        <Link to="/settings">
+          {photo.img ? (
+            <img
+              className="avatar"
+              src={photo.img}
+              alt={username}
+              title={username}
+            />
+          ) : (
+            <img
+              className="avatar"
+              src="https://res.cloudinary.com/dw8wf8gps/image/upload/v1637507302/default-text-effect_67638-192_sgvqyk.jpg"
+              alt={username}
+              title={username}
+            />
+          )}
+        </Link>
       </li>
       <li className="btnAdd">
         <Link to="/create">مقاله جدید</Link>
