89c6c194eac07b9bf143eebbadf0fc1ad103c43e
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-22 19:06:13 +0330

    update user route - edit user

--- server/Routes/Users.js
+++ server/Routes/Users.js
@@ -2,9 +2,15 @@ const { Router } = require("express");
 const userController = require("../Controllers/Users");
 const { checkUser } = require("../MiddleWares/common");
 const router = Router();
+const upload = require("../utils/multer");
 
 router.delete("/remove/:id", checkUser, userController.removeUser_delete);
-router.put("/edit/:id", checkUser, userController.editUser_put);
+router.put(
+  "/edit/:username",
+  checkUser,
+  upload.single("photo"),
+  userController.editUser_put
+);
 router.get("/get/:id", userController.getUserById_get);
 
 module.exports = router;
