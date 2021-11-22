d1c44ae74bbd018a7a3e58ee1538bb85b2aba042
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-22 19:05:28 +0330

    add userApi - CRUD redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { postsFetch } from "./postsSlice";
 import { postsApi } from "./postsApi";
 import { catsApi } from "./catsApi";
+import { usersApi } from "./userApi";
 import { authSlice } from "./authSlice";
 export const store = configureStore({
   reducer: {
@@ -10,12 +11,14 @@ export const store = configureStore({
     //auth: authReducer,
     user: authSlice.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
     [catsApi.reducerPath]: catsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(postsApi.middleware)
-      .concat(catsApi.middleware),
+      .concat(catsApi.middleware)
+      .concat(usersApi.middleware),
 });
 
 store.dispatch(postsFetch());
---
+++ client/src/redux/userApi.js
@@ -0,0 +1,53 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const usersApi = createApi({
+  reducerPath: "usersApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/api/user",
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    getAllUsers: builder.query({
+      query: (search) => ({ url: `getall/${search}` }),
+    }),
+    getUserById: builder.query({
+      query: (id) => `getall?cat=${id}`,
+    }),
+    getUserByUsername: builder.query({
+      query: (username) => `getall?user=${username}`,
+    }),
+    addUser: builder.mutation({
+      query: (user) => ({
+        url: `add`,
+        method: "POST",
+        body: user,
+      }),
+    }),
+    editUser: builder.mutation({
+      query: ({ username, userData }) => ({
+        url: `edit/${username}`,
+        method: "PUT",
+        body: userData,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `remove/${id}`,
+        method: "Delete",
+      }),
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {
+  useGetAllUsersQuery,
+  useGetUserByIdQuery,
+  useGetUserByUsernameQuery,
+  useAddUserMutation,
+  useEditUserMutation,
+  useDeleteUserMutation,
+} = usersApi;
