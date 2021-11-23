5e91619a5dea2d4cfe9d27b7bd68077d2e600ba4
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-23 21:27:44 +0330

    load post data in parent

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -2,11 +2,21 @@ import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import SinglePost from "./SinglePost";
 import "./Article.css";
+import { useLocation } from "react-router-dom";
 
+import { useGetPostByIdQuery } from "../../redux/postsApi";
 function Article({ username, description, photo }) {
+  const location = useLocation();
+  console.log(location);
+  const id = location.pathname.split("/")[2];
+  const {
+    data,
+    // error, isLoading
+  } = useGetPostByIdQuery(id);
+
   return (
     <div className="article">
-      <SinglePost username={username} />
+      {data && <SinglePost data={data} id={id} username={username} />}
       <Sidebar />
     </div>
   );
