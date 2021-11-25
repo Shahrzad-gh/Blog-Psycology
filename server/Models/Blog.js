a53c0950d37ded000d8ba8fc71e581bf25fda0db
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-25 19:21:09 +0330

    add blog model

--- server/Models/Blog.js
+++ server/Models/Blog.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-  title: {},
-  name: {},
-  about: {},
-  photo: {},
-  socialLinks: {},
+  title: {
+    type: String,
+  },
+  subTitle: { type: String },
+  name: { type: String },
+  photo: {
+    img: { type: String },
+    id: { type: String },
+  },
+  about: { type: String },
+  socialLinks: {
+    instagram: { type: String },
+    facebook: { type: String },
+    twitter: { type: String },
+  },
 });
 
 const Blog = mongoose.model("blog", BlogSchema);
