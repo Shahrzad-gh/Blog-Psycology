69169d72050b9f4b6fb3d2ca0fbe92918ef1a3e4
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-25 19:20:34 +0330

    load admin - to edit site info

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -4,11 +4,21 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import { useEditUserMutation } from "../redux/userApi";
 import { useHistory } from "react-router-dom";
 
-function Settings({ username, email, photo, description }) {
+function Settings({
+  username,
+  email,
+  photo,
+  description,
+  role,
+  instagram,
+  facebook,
+  twitter,
+  about,
+}) {
   const [picture, setPicture] = useState();
   const [url, setUrl] = useState("");
   const history = useHistory();
-
+  console.log(role);
   function handleUploadImage(e) {
     setPicture(e.target.files[0]);
     setUrl(URL.createObjectURL(e.target.files[0]));
@@ -19,7 +29,6 @@ function Settings({ username, email, photo, description }) {
     password: "",
     description: description,
   });
-
   const handleOnChange = (e) => {
     setUserInfo({
       ...userInfo,
@@ -41,13 +50,36 @@ function Settings({ username, email, photo, description }) {
       window.location.reload();
     });
   };
+  const [site, setSite] = useState({
+    instagram: instagram,
+    facebook: facebook,
+    twitter: twitter,
+    aboutSite: about,
+  });
+  const handleOnSiteChange = (e) => {
+    setSite({ ...site, [e.target.name]: e.target.value });
+  };
+  const handleSiteSettings = (e) => {
+    e.preventDefault();
+    const siteData = new FormData();
+    siteData.append("instagram", site.instagram);
+    siteData.append("facebook", site.facebook);
+    siteData.append("twitter", site.twitter);
+    siteData.append("aboutSite", site.aboutSite);
+    trigger({ role, siteData }).then(() => {
+      history.push(`/settings`);
+      window.location.reload();
+    });
+  };
 
   return (
     <div className="settings">
       <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="updateAccount">به روز رسانی حساب کاربری</span>
-          {/* <span className="deleteAccount">پاک کردن حساب کاربری</span> */}
+          {/* {role === "admin" ? (
+            <span className="deleteAccount">پاک کردن حساب کاربری</span>
+          ) : null} */}
         </div>
         <form className="settingsForm" onSubmit={handleSubmit}>
           <div className="settingsProfilePicture">
@@ -86,7 +118,7 @@ function Settings({ username, email, photo, description }) {
             />
           </div>
           <div className="form-settings">
-            <div>
+            <div className="userSetting">
               <label htmlFor="username">نام کاربری</label>
               <input
                 type="text"
@@ -126,43 +158,55 @@ function Settings({ username, email, photo, description }) {
                 به روز رسانی
               </button>
             </div>
-            <div>
-              {/* <label htmlFor="">شبکه های اجتماعی</label> */}
-              <label htmlFor="instagram">اینستاگرام</label>
-              <input
-                style={{ width: "30vw" }}
-                type="text"
-                id="instagram"
-                name="instagram"
-                onChange={handleOnChange}
-              />
-              <label htmlFor="instagram">فیسبوک</label>
+            {role === "admin" ? (
+              <form className="adminSetting" onSubmit={handleSiteSettings}>
+                <label style={{ marginBottom: " 5px" }} htmlFor="instagram">
+                  اینستاگرام
+                </label>
+                <input
+                  style={{ width: "30vw" }}
+                  type="text"
+                  id="instagram"
+                  name="instagram"
+                  value={site.instagram}
+                  onChange={handleOnSiteChange}
+                />
+                <label htmlFor="instagram">فیسبوک</label>
 
-              <input
-                style={{ width: "30vw" }}
-                type="text"
-                id="facebook"
-                name="facebook"
-                onChange={handleOnChange}
-              />
-              <label htmlFor="instagram">توئیتر</label>
+                <input
+                  style={{ width: "30vw" }}
+                  type="text"
+                  id="facebook"
+                  name="facebook"
+                  value={site.facebook}
+                  onChange={handleOnSiteChange}
+                />
+                <label htmlFor="instagram">توئیتر</label>
 
-              <input
-                style={{ width: "30vw" }}
-                type="text"
-                id="twitter"
-                name="twitter"
-                onChange={handleOnChange}
-              />
+                <input
+                  style={{ width: "30vw" }}
+                  type="text"
+                  id="twitter"
+                  name="twitter"
+                  value={site.twitter}
+                  onChange={handleOnSiteChange}
+                />
 
-              <label htmlFor="desc">درباره سایت</label>
-              <textarea
-                id="about"
-                maxLength="60"
-                name="about"
-                onChange={handleOnChange}
-              />
-            </div>
+                <label htmlFor="desc">درباره سایت</label>
+                <textarea
+                  id="about"
+                  maxLength="60"
+                  name="about"
+                  value={site.aboutSite}
+                  onChange={handleOnSiteChange}
+                />
+                <button type="submit" className="siteSubmit">
+                  به روز رسانی مشخصات سایت
+                </button>
+              </form>
+            ) : (
+              <div></div>
+            )}
           </div>
         </form>
       </div>
