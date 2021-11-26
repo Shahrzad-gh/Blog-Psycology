2ba08cb8c6bc8742c727d5cad4c2dc4b9e49b113
Shahr Zad <gh.shahrzad@gmail.com>
2021-11-26 18:32:03 +0330

    add blog to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,21 +4,26 @@ import { postsFetch } from "./postsSlice";
 import { postsApi } from "./postsApi";
 import { catsApi } from "./catsApi";
 import { usersApi } from "./userApi";
+import { blogApi } from "./blogApi";
 import { authSlice } from "./authSlice";
+import { blogSlice } from "./blogSlice";
 export const store = configureStore({
   reducer: {
     //posts: postsReducer,
     //auth: authReducer,
     user: authSlice.reducer,
+    blog: blogSlice.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [catsApi.reducerPath]: catsApi.reducer,
+    [blogApi.reducerPath]: blogApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(postsApi.middleware)
       .concat(catsApi.middleware)
-      .concat(usersApi.middleware),
+      .concat(usersApi.middleware)
+      .concat(blogApi.middleware),
 });
 
 store.dispatch(postsFetch());
