e85e46a5e8e903d8b6db416e30c36aee4455da3f
Shahr Zad <gh.shahrzad@gmail.com>
2021-12-11 12:53:59 +0330

    css responsive navbar update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import SignInLinks from "./SignInLinks";
 import SignOutLinks from "./SignOutLinks";
 import { Link } from "react-router-dom";
 
 export default function Navbar({ user, username, photo }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMobileMenu = () => {
+    setIsOpen(!isOpen);
+    isOpen ? handleCloseMobileMenu() : handleOpenMobileMenu();
+  };
+
+  const handleOpenMobileMenu = () => {
+    document.querySelector(".menu-icon").innerHTML =
+      "<i class='fas fa-times'></i>";
+    document.querySelector(".navbar").style.display = "block";
+    document.querySelector(".mobile-links").style.display = "block";
+  };
+
+  const handleCloseMobileMenu = () => {
+    document.querySelector(".navbar").style.display = "none";
+    document.querySelector(".mobile-links").style.display = "none";
+    document.querySelector(".menu-icon").innerHTML =
+      "<i class='fas fa-bars'></i>";
+  };
+
   return (
     <div className="nav">
+      <a href={void 0} className="menu-icon" onClick={handleMobileMenu}>
+        <i className="fa fa-bars"></i>
+      </a>
       <div className="navbar">
         <ul>
-          <li className="menu-icon">
-            <i className="fas fa-bars"></i>
-          </li>
           <li>
             <Link to="/">خانه</Link>
           </li>
@@ -21,6 +42,14 @@ export default function Navbar({ user, username, photo }) {
           <li>
             <Link to="/contact"> تماس با ما</Link>
           </li>
+          <div className="mobile-links">
+            <li>
+              <Link to="/create">مقاله جدید</Link>
+            </li>
+            <li>
+              <Link to="/signout"> خروج </Link>
+            </li>
+          </div>
         </ul>
       </div>
       <div className="links">
