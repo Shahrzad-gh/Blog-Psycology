f3d6eb6e856701c20d65fdffedff7bb275ae1597
Shahr Zad <gh.shahrzad@gmail.com>
2021-12-14 11:49:56 +0330

    css media queries update

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <div>404</div>;
+  return (
+    <div className="not-found">
+      <div className="not-found-titles">
+        <span className="not-found-titleLg">404</span>
+
+        <span className="not-found-titleSm">
+          به نظر می‌رسد که شما در بین صفحات گم شده اید، ما به شما کمک می کنیم تا
+          از اینجا به بیرون بروید.
+        </span>
+      </div>
+      <Link to="/">
+        <button className="go-home">بازگشت به صفحه اصلی</button>
+      </Link>
+      <img className="not-found-img" src="" alt="" title="" />
+    </div>
+  );
 }
 
 export default NotFound;
